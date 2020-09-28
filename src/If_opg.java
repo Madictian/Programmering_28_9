48d4d1be06258d00700e8f5f469edd312aa425b8
Madictian <fredrikbille@hotmail.com>
2020-09-28 09:28:15 +0200

    Initial commit

---
+++ src/If_opg.java
@@ -0,0 +1,14 @@
+public class If_opg {
+    public static void main(String[] args) {
+        int interest = (int)(Math.random() * 3 + 1);
+
+        if (interest == 3)
+            System.out.print("meget interesseret");
+        else if (interest == 2)
+            System.out.print("til stede");
+        else
+            System.out.print("uhh, sommerfugl!");
+
+    }
+
+}
---
+++ src/Switch_opg.java
@@ -0,0 +1,23 @@
+public class Switch_opg {
+    public static void main(String[] args) {
+        int nice = (int)(Math.random() * 5 + 1);
+
+        switch (nice){
+            case 1:
+            System.out.print("meh");
+            break;
+            case 2:
+            System.out.print("okey, i guess");
+                break;
+            case 3:
+            System.out.print("nice");
+            break;
+            case 4:
+            System.out.print("cool");
+            break;
+            case 5:
+            System.out.print("supercalifragilisticexpialidocious");
+            break;
+        }
+    }
+}
