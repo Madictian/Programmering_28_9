8dfffa964f85d163960f7b99c9df6a62106c5bf9
Madictian <fredrikbille@hotmail.com>
2020-09-29 13:15:38 +0200

    mere arbejde.

---
+++ src/Smart_table.java
@@ -0,0 +1,9 @@
+public class Smart_table {
+    public static void main(String[] args) {
+        //ints til programmet kunne være input fra sensorer
+        //en bi weekly notification om at tørre bordet af
+        //Forskellige modes/profiles Workspace, gaming rig, dinnertable, coffetable.
+        //indstillinger for Skygge følsomhed og/eller glidende følsomhed afhængig af tidspunkt.
+        //
+    }
+}
